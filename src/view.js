238346b6ca090e08ff2e26bdc37b482e03c1326f
belato <sola.belator@gmail.com>
2025-10-04 19:09:22 +0300

    add view.js

---
+++ src/view.js
@@ -0,0 +1,95 @@
+import { string } from 'yup'
+import onChange from 'on-change'
+
+const feedback = document.querySelector('.feedback')
+const form = document.querySelector('.rss-form')
+
+const state = {
+  valid: '',
+  feeds: [],
+  message: '',
+}
+
+const watchedObject = onChange(state, function (path, value, previousValue) {
+  return { path, value, previousValue }
+})
+
+function validator (str) {
+  return new Promise((resolve) => {
+    let schema = string().url().nullable()
+
+    if (state.feeds.includes(str)) {
+      resolve ({
+        valid: 'exist',
+        message: 'RSS уже существует',
+      })
+      return
+    }
+    schema.isValid(str)
+      .then(data => {
+        if (data === false) {
+          resolve ({
+            valid: false,
+            message: 'Ссылка должна быть валидным URL'
+          })
+          return 
+        }
+        else {
+          state.feeds.push(str)
+          resolve ({
+            valid: true,
+            message: '',
+          })
+        }
+    })
+    .catch(error => {
+      resolve({
+        valid: false,
+        message: 'Ошибка при проверке URL'
+      })
+    })
+  })
+}
+
+function render () {
+  const input = document.getElementById('url-input')
+
+  input.classList.remove('is-invalid')
+  feedback.innerHTML = ''
+  switch (watchedObject.valid) {
+    case false: {
+      input.classList.add('is-invalid')
+      break
+    }
+    case true: {
+      form.reset()
+      input.focus()
+      break
+    }
+    case 'exist': {
+      input.classList.add('is-invalid')
+      break
+    }
+    default: {
+      break
+    }
+  }
+
+  feedback.textContent = watchedObject.message
+}
+
+export default function view() {
+  form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const formInput = e.target.querySelector('#url-input')
+  const url = formInput.value.trim()
+  render()
+
+  validator(url)
+    .then(validationData => {
+      watchedObject.valid = validationData.valid
+      watchedObject.message = validationData.message
+      render()
+    })
+  })
+}
