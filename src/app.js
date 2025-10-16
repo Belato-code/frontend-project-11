c0d0a28aae7ba94235cfe2111616e6604431d2c2
belato <sola.belator@gmail.com>
2025-10-16 21:52:21 +0300

    add posts and feeds

--- src/app.js
+++ src/app.js
@@ -2,18 +2,46 @@ import * as yup from 'yup'
 import i18next from 'i18next'
 import resources from './locales/index.js'
 import watch from './view.js'
+import axios from 'axios'
+import parser from './parser.js'
+
+const makeURL = (link) => {
+  const url = new URL('get', 'https://allorigins.hexlet.app')
+  url.searchParams.append('disableCash', 'true')
+  url.searchParams.append('url', link)
+
+  return url.toString()
+}
 
 export default () => {
   const state = {
     existingUrls: [],
     status: 'filling',
     error: null,
+    feeds: [],
+    posts: [],
   }
 
   const elements = {
     form: document.querySelector('.rss-form'),
     feedback: document.querySelector('.feedback'),
-    input: document.getElementById('url-input'),
+    input: document.querySelector('#url-input'),
+    posts: document.querySelector('.posts'),
+    feeds: document.querySelector('.feeds'),
+  }
+
+  const getData = (url, watchedState, i18n) => {
+    return axios.get(makeURL(url))
+      .then((response) => {
+        const parsedData = parser(response, i18n)
+        return parsedData
+      })
+      .then((parsedData) => {
+        watchedState.feeds.push(parsedData.feed)
+        watchedState.posts.push(parsedData.posts)
+        return parsedData
+      })
+      .catch(error => console.log(error.message))
   }
   const i18n = i18next.createInstance()
   i18n.init({
@@ -21,49 +49,53 @@ export default () => {
     debug: false,
     resources,
   }).then(() => {
+      yup.setLocale({
+        string: {  
+          url: () => i18n.t('errors.url'),
+        },
+        mixed: {
+          notOneOf: () => i18n.t('errors.exist'),
+          required: () => i18n.t('errors.empty'),
+        },
+      })
 
-    yup.setLocale({
-      string: {  
-        url: () => i18n.t('errors.url'),
-      },
-      mixed: {
-        notOneOf: () => i18n.t('errors.exist'),
-        required: () => i18n.t('errors.empty'),
-      },
-    })
-
-    const watchedStatus = watch(state, elements, i18n)
-    const validateURL = (existingUrls, url) => {
-      const schema = yup.string().url().required().notOneOf(existingUrls)
-      return schema
-        .validate(url)
-        .then(() => ({ isValid: true, message: null }))
-        .catch((error) => ({
-          isValid: false,
-          message: error.message
-        }))
-    }
-    elements.form.addEventListener('submit', (e) => {
-      e.preventDefault()
-      const url = elements.input.value.trim()
-      validateURL(watchedStatus.existingUrls, url)
-        .then((validationResult) => {
-          if (validationResult.isValid) {
-            watchedStatus.status = 'valid'
-            watchedStatus.error = null
-            watchedStatus.existingUrls.push(url)
-            elements.form.reset()
-          }
-          else {
-            watchedStatus.status = 'error'
-            watchedStatus.error = validationResult.message
-            elements.input.focus()
-          }
-        }).catch((error) => {
-          state.status = 'error'
-          state.error = i18n.t('errors.unknown')
-          console.error('Validation promise failed:', error)
-        })
-    })
+      const watchedState = watch(state, elements, i18n)
+      const validateURL = (existingUrls, url) => {
+        const schema = yup.string().url().required().notOneOf(existingUrls)
+        return schema
+          .validate(url)
+          .then(() => ({ isValid: true, message: null }))
+          .catch((error) => ({
+            isValid: false,
+            message: error.message
+          }))
+      }
+      elements.form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        const url = elements.input.value.trim()
+        validateURL(watchedState.existingUrls, url)
+          .then((validationResult) => {
+            if (validationResult.isValid) {
+              return getData(url, watchedState, i18n)
+                .then(() => {
+                  console.log(watchedState.feeds)
+                  watchedState.error = null
+                  watchedState.status = 'valid'
+                  watchedState.existingUrls.push(url)
+                  elements.form.reset()
+                })
+            }
+            else {
+              watchedState.status = 'error'
+              watchedState.error = validationResult.message
+              elements.input.focus()
+            }
+          })
+          .catch((error) => {
+            watchedState.status = 'invalidRSS'
+            watchedState.error = error.message
+            console.error('Request failed:', error)
+          })
+      })
   })
 }
--- src/view.js
+++ src/view.js
@@ -1,7 +1,7 @@
 import onChange from "on-change"
 
 export default (state, elements, i18n) => {
-  const { feedback, input } = elements
+  const { feedback, input, posts, feeds } = elements
 
   const handleFeedback = () => {
     input.classList.remove('is-valid', 'is-invalid')
@@ -17,9 +17,94 @@ export default (state, elements, i18n) => {
         feedback.textContent = i18n.t('success')
         input.classList.add('is-valid')
         break
+      case 'invalidRSS':
+        feedback.textContent = state.error
+        feedback.classList.add('text-danger')
+        break
       default:
         break
     }
+  }
+
+  const preparePosts = (posts) => {
+    const lists = document.createElement('ul')
+    lists.classList.add('list-group')
+
+    posts.forEach((post) => {
+      const li = document.createElement('li')
+      li.classList.add('list-group-item', 'd-flex', 'justify-content-between', 'rounded-0', 'border-0')
+
+      const link = document.createElement('a')
+      link.classList.add('fw-bold')
+      link.href = post.link
+      link.textContent = post.title
+
+      const button = document.createElement('button')
+      button.classList.add('btn', 'btn-outline-primary')
+      button.textContent = i18n.t('content.button')
+      
+      li.append(link, button)
+      lists.append(li)
+    })
+
+    return lists
+  }
+
+  const renderPosts = () => {
+    posts.innerHTML = ''
+    const card = document.createElement('div')
+    card.classList.add('card', 'border-0')
+
+    const cardBody = document.createElement('div')
+    cardBody.classList.add('card-body')
+    const postsTitle = document.createElement('h2')
+    postsTitle.classList.add('card-title', 'h4')
+    postsTitle.textContent = i18n.t('content.posts')
+    cardBody.append(postsTitle)
+
+    card.append(cardBody)
+
+    state.posts.forEach(element => {
+      const post = preparePosts(element)
+      card.append(post)
+    })
+    posts.append(card)
+  }
+
+  const renderFeeds = () => {
+    feeds.innerHTML = ''
+    const container = document.createElement('div')
+    container.classList.add('card', 'border-0', 'rounded-0')
+
+    const div = document.createElement('div')
+    div.classList.add('card-body', 'border-0', 'rounded-0')
+    const title = document.createElement('h2')
+    title.classList.add('card-title', 'h4')
+    title.textContent = i18n.t('content.feeds')
+    div.append(title)
+
+    container.append(div)
+
+    state.feeds.forEach((feed) => {
+      const ul = document.createElement('ul')
+      ul.classList.add('list-group')
+
+      const li = document.createElement('li')
+      li.classList.add('list-group-item', 'rounded-0', 'border-0')
+
+      const title = document.createElement('h3')
+      title.classList.add('h6')
+      title.textContent = feed.title
+
+      const subtitle = document.createElement('p')
+      subtitle.classList.add('m-0', 'text-black-50')
+      subtitle.textContent = feed.description
+
+      li.append(title, subtitle)
+      ul.append(li)
+      container.append(ul)
+    })
+    feeds.append(container)
   }
 
   const watchedState = onChange(state, (path) => {
@@ -27,6 +112,12 @@ export default (state, elements, i18n) => {
       case 'status':
       case 'error':
         handleFeedback()
+        break
+      case 'posts':
+        renderPosts()
+        break
+      case 'feeds':
+        renderFeeds()
         break
       default:
         break
