4014d9f33eb146ae26340ba56ace7a4ef73c0fa0
belato <sola.belator@gmail.com>
2025-10-16 21:51:36 +0300

    update update html and locales

--- src/locales/ru.js
+++ src/locales/ru.js
@@ -4,8 +4,14 @@ export default {
       url: 'Ссылка должна быть валидным URL',
       empty: 'Поле не должно быть пустым',
       exist: 'RSS уже существует',
-      unknown: 'Ошибка валидации'
+      unknown: 'Ошибка валидации',
+      parserError: 'Ресурс не содержит валидный RSS'
     },
-    success: 'RSS успешно загружен'
+    success: 'RSS успешно загружен',
+    content: {
+      feeds: 'Фиды',
+      posts: 'Посты',
+      button: 'Просмотр'
+    }
   }
 }
